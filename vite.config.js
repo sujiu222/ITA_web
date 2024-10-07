9de2f4c7ccf639618eb09449ffbaa40127385ee6
sujiu <2994338151@qq.com>
2024-10-07 15:16:52 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/vue3-music/',
-  build: {
-    outDir: 'docs'
-  }
+  base: '/shetuan/',
+  plugins: [vue()]
 })
